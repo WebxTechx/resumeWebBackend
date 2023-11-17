0bc4ca39a7e779a5fbc81c83b4718a23b12e5bd2
Tarun Gupta <99278334+WebxTechx@users.noreply.github.com>
2023-11-18 00:59:39 +0530

    login func created

--- src/controllers/v1/users.controller.js
+++ src/controllers/v1/users.controller.js
@@ -1,42 +1,107 @@
-import {User} from "../../models/user.model.js"
+import { User } from "../../models/user.model.js";
+import { ApiError } from "../../utils/ApiError.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
 
 // Get all users
 const getUsers = async (req, res) => {
-    try {
-      const users = await User.find(); // Fetch all users from the database
-  
-      res.status(200).json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
+  try {
+    const users = await User.find(); // Fetch all users from the database
 
-  const createUser = async (req, res) => {
-    const { fullName, email,first_name,last_name,password } = req.body;
-  
-    try {
-      // Validate input
-      if (!fullName || !email || !first_name ||!last_name || !password) {
-        return res.status(400).json({ error: 'fullName and email are required' });
-      }
-  
-      // Check if the user with the same email already exists
-      const existingUser = await User.findOne({ email });
-  
-      if (existingUser) {
-        return res.status(400).json({ error: 'User with this email already exists' });
-      }
-  
-      // Create a new user
-      const newUser = new User({ fullName, email, first_name, last_name, password });
-      console.log(newUser);
-      await newUser.save();
-  
-      res.status(201).json(newUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Create user
+const createUser = async (req, res) => {
+  const { fullName, email, first_name, last_name, password } = req.body;
+
+  try {
+    // Validate input
+    if (!fullName || !email || !first_name || !last_name || !password) {
+      return res.status(200).json({ error: "fullName and email are required" });
     }
-  };
-export {getUsers,createUser}
+
+    // Check if the user with the same email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(200)
+        .json({ error: "User with this email already exists" });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      fullName,
+      email,
+      first_name,
+      last_name,
+      password,
+    });
+
+    await newUser.save();
+
+    res
+      .status(201)
+      .json(new ApiResponse(201, newUser, "User created successfully"));
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+      errors: error.errors || [],
+    });
+  }
+};
+
+
+// User Login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Validate input
+    if (!email || !password) {
+      return res.status(200).json({ error: "Email and Password are required" });
+    }
+
+    // check existing user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, "", "User does not exist"));
+    }
+
+    if (!(user && (await user.isPasswordCorrect(password)))) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, "", "Password entered is not valid"));
+    }
+
+    const sanitizedUser = {
+      _id: user._id,
+      email: user.email,
+      uuid: user.uuid,
+    };
+
+    if (user && (await user.isPasswordCorrect(password))) {
+      return res
+        .status(200)
+        .header("Authorization", `Bearer ${await user.generateAccessToken()}`)
+        .json(
+          new ApiResponse(200, sanitizedUser, "User loggedin successfully")
+        );
+    }
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+      errors: error.errors || [],
+    });
+  }
+};
+
+export { getUsers, createUser, loginUser };
--- src/routes/v1/v1.route.js
+++ src/routes/v1/v1.route.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {getUsers,createUser} from "../../controllers/v1/users.controller.js";
+import {getUsers,createUser,loginUser} from "../../controllers/v1/users.controller.js";
 const v1Routes = express.Router();
 
 // Define v1 routes
-v1Routes.get('/users', createUser);
+v1Routes.get('/users', getUsers);
+v1Routes.post('/users', createUser);
+v1Routes.post('/users/login', loginUser);
 
 export {v1Routes}
