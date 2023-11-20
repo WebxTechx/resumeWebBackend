46b043d739d1d8fde768b69d42875ea733e157e6
Tarun Gupta <99278334+WebxTechx@users.noreply.github.com>
2023-11-21 00:54:10 +0530

    middleware check token and isadmin

--- src/routes/v1/v1.route.js
+++ src/routes/v1/v1.route.js
@@ -1,10 +1,16 @@
-import express from "express"
-import {getUsers,createUser,loginUser} from "../../controllers/v1/users.controller.js";
+import express from "express";
+import { checkTokenMiddleware } from "../../middlewares/authTokenVerify.middleware.js";
+import { checkIsAdmin } from "../../middlewares/isAdmin.middleware.js";
+import {
+  getUsers,
+  createUser,
+  loginUser,
+} from "../../controllers/v1/users.controller.js";
 const v1Routes = express.Router();
 
 // Define v1 routes
-v1Routes.get('/users', getUsers);
-v1Routes.post('/users', createUser);
-v1Routes.post('/users/login', loginUser);
+v1Routes.get("/users", checkTokenMiddleware, checkIsAdmin, getUsers);
+v1Routes.post("/users", createUser);
+v1Routes.post("/users/login", loginUser);
 
-export {v1Routes}
+export { v1Routes };
