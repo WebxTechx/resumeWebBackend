22b413265a44995a45c51564189749b09167fa24
Tarun Gupta <tg.whiteforce@gmail.com>
2023-11-25 16:41:12 -0800

    correction

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,9 +2,9 @@ import express from "express";
 const mainRouter = express.Router();
 
 // Import the v1 routes
-import { v1Routes } from "./v1/v1.route.js";
+import router from "./v1/v1.route.js";
 
 // Use the v1 routes
-mainRouter.use("/v1", v1Routes);
+mainRouter.use("/v1", router);
 
 export default mainRouter;
